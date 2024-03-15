b623466f9465a7b33d00263fbf0a55d41d28520b
vnminhhai <vmhtl2004@gmail.com>
2024-03-16 02:54:58 +0700

    apply filter

--- OnmazaEcommerce/src/java/filter/LoginFilter.java
+++ OnmazaEcommerce/src/java/filter/LoginFilter.java
@@ -20,7 +20,10 @@ public class LoginFilter implements Filter{
     @Override
     public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest)sr;
-        if (request.getSession().getAttribute("customer")==null) throw new ServletException("Login required.");
+        if (request.getSession().getAttribute("customer")==null) {
+            request.getSession().setAttribute("current", request.getRequestURL().toString());
+            request.getRequestDispatcher("login.jsp").forward(request, sr1);
+        }
         else fc.doFilter(sr, sr1);
     }
 }
